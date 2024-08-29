8136f7ba94a430fea520e62fd77c92b861201ec1
nesiagoodloe <157249824+nesiagoodloe@users.noreply.github.com>
2024-08-28 19:50:53 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const app = express()
 console.log('im on a node Server, yo');
 
 app.get('/', function (req, res){
-res.send('Hello Node from local Devbox')
+//res.send('Hello Node from local Devbox')
+res.sendFile('index.html');
 
 })
 app.listen(3000);
