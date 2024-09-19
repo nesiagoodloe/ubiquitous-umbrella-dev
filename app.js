22b5e4914e2cd2bc112c63048f65fa04d97b1a61
nesiagoodloe <157249824+nesiagoodloe@users.noreply.github.com>
2024-09-19 00:09:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express()
 console.log('im on a node server, yo');
 
 app.get('/', function (req, res) {
-  res.send('Hello Node from Ex on local dev box')
+   res.send('Hello Node from Ex on local dev box')
+  res.sendFile('index.html');
 })
 
 app.listen(3000)
